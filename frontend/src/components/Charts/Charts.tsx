50bdee0b0a76396a6b86ad329c30904e1d7a5d56
Rob <king.roob@gmail.com>
2024-07-17 01:29:20 +0100

    feat: add react-gauge-chart for battery level indicator

--- frontend/src/components/Charts/Charts.tsx
+++ frontend/src/components/Charts/Charts.tsx
@@ -7,6 +7,7 @@ import {
     XAxis,
     YAxis,
 } from "recharts";
+import GaugeChart from 'react-gauge-chart'
 
 import { useSensorContext } from "../../context/SensorContext";
 
@@ -19,23 +20,26 @@ export default function Charts() {
     }
     const { timestamp, gpsCoordinates, chartData, altitudeUnit, ...rest } = sensorData;
     return (
-        <div className="charts">
-            {Object.keys(rest).map((key, index) => (
-                <LineChart width={600} height={300} data={sensorData.chartData} key={index}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="timestamp" />
-                    <YAxis yAxisId="left" />
-                    <YAxis yAxisId="right" orientation="right" />
-                    <Tooltip />
-                    <Legend />
-                    <Line
-                        yAxisId="left"
-                        type="monotone"
-                        dataKey={key}
-                        stroke={chartColors[index]}
-                    />
-                </LineChart>
-            ))}
+        <div>
+            <GaugeChart id="gauge-chart1" percent={sensorData.batteryLevel / 100} animate={false} nrOfLevels={30} colors={["#DB0032", "#ff6d2b", "#66ec0a"]} style={{ height: 250, width: 250 }} />
+            <div className="charts">
+                {Object.keys(rest).map((key, index) => (
+                    <LineChart width={600} height={300} data={sensorData.chartData} key={index}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="timestamp" />
+                        <YAxis yAxisId="left" />
+                        <YAxis yAxisId="right" orientation="right" />
+                        <Tooltip />
+                        <Legend />
+                        <Line
+                            yAxisId="left"
+                            type="monotone"
+                            dataKey={key}
+                            stroke={chartColors[index]}
+                        />
+                    </LineChart>
+                ))}
+            </div>
         </div>
     );
 }
