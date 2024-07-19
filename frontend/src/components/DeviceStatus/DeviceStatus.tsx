464d7e45da9617d93fe7b134b68002c88aa93e90
Rob <king.roob@gmail.com>
2024-07-19 08:08:24 +0100

    fix: update styling of gauges

--- frontend/src/components/DeviceStatus/DeviceStatus.tsx
+++ frontend/src/components/DeviceStatus/DeviceStatus.tsx
@@ -7,12 +7,12 @@ export default function Charts() {
     const { batteryLevel, speed } = sensorData;
     return (
         <div className='gauge-container'>
-            <div className='data-item'>
-                <h2>Battery</h2>
+            <div className='gauge-item'>
+                <span className='label'>Battery</span>
                 <GaugeChart id="gauge-chart1" percent={batteryLevel / 100} animate={false} nrOfLevels={30} colors={["#DB0032", "#ff6d2b", "#66ec0a"]} style={{ height: 100, width: 200 }} />
             </div>
-            <div className='data-item'>
-                <h2>Speed</h2>
+            <div className='gauge-item'>
+                <span className='label'>Speed</span>
                 <GaugeChart id="gauge-chart1" formatTextValue={(speed) => `${speed} m/s`} percent={speed / 100} animate={false} nrOfLevels={30} colors={["#66ec0a", "#ff6d2b", "#DB0032"]} style={{ height: 100, width: 200 }} />
             </div>
         </div>
